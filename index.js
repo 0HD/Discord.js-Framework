e16ab2400f0cc1429f6094d1c3572052136c1884
0HD <64617192+0HD@users.noreply.github.com>
2020-11-15 12:41:29 +0300

    Added !ping command and config file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const Discord = require('discord.js');
 
 // create a new Discord client
+const config = require('./config.json');
 const client = new Discord.Client();
 
 // when the client is ready, run this code
@@ -10,5 +11,16 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
+// Sends any message it sees to the console
+client.on('message', message => {
+    console.log(`(MESSAGE FROM ${message.author}) ${message.content}`);
+    
+    if (message.content === '!ping') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+});
+
+
 // login to Discord with your app's token
-client.login('your-token-goes-here');
+client.login(config.token);
