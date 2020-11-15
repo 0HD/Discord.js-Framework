b0c464591944bf193ed40ea7e917731ed48936d8
0HD <64617192+0HD@users.noreply.github.com>
2020-11-15 13:02:08 +0300

    Improved config load method, improved comments

--- index.js
+++ index.js
@@ -1,26 +1,35 @@
+console.log("Starting...")
+
 // require the discord.js module
 const Discord = require('discord.js');
 
 // create a new Discord client
-const config = require('./config.json');
+const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+    // prints to the console when it's ready
+    console.log('Ready.');
 });
+////
 
-// Sends any message it sees to the console
+// when the client receives a message
 client.on('message', message => {
+    // prints messages to the console
     console.log(`(MESSAGE FROM ${message.author}) ${message.content}`);
-    
-    if (message.content === '!ping') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('Pong.');
+    ////
+
+    // sends a response message when someone sends a command
+    if (message.content === (`${prefix}ping`)) {
+        message.channel.send('Pong.'); // response to "!ping"
+    } else if (message.content === (`${prefix}beep`)) {
+        message.channel.send('Boop.'); // response to "!beep"
     }
+    ////
 });
 
 
-// login to Discord with your app's token
-client.login(config.token);
+// login to discord with your bot token
+client.login(token);
