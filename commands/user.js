d3e5168e4fc5345d42e48d39433d324022752102
0HD <64617192+0HD@users.noreply.github.com>
2020-11-16 21:00:19 +0300

    Delete user.js

--- commands/user.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'user',
-    description: 'User information',
-    execute(message, args) {
-        message.channel.send(`Your name: ${message.author.username}\n
-        Your ID: ${message.author.id}`);
-    },
-};
