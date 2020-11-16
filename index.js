45b7272c03b15e8d704353d773794a3a585af20c
0HD <64617192+0HD@users.noreply.github.com>
2020-11-16 13:43:33 +0300

    Fixed Undefined error when using !kick without a mention

--- index.js
+++ index.js
@@ -59,8 +59,10 @@ client.on('message', message => {
         // grab the "first" mentioned user from the message
         // this will return a `User` object, just like `message.author`
         const taggedUser = message.mentions.users.first();
-
-        message.channel.send(`Kicked ${taggedUser} (demo)`);
+        if (!message.mentions.users.size) {
+            return message.reply('you need to tag a user in order to kick them!');
+        }
+        else {message.channel.send(`Kicked ${taggedUser} (demo)`);}
     }
 
     ////
