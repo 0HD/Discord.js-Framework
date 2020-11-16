fcda681f7c00fc9d61f25183c4f0f43ece69447e
0HD <64617192+0HD@users.noreply.github.com>
2020-11-16 13:48:41 +0300

    Added "avatar" command Sends the user's or the mentioned users' avatars in one message

--- index.js
+++ index.js
@@ -58,11 +58,24 @@ client.on('message', message => {
     else if (command === 'kick') { // "kick" command
         // grab the "first" mentioned user from the message
         // this will return a `User` object, just like `message.author`
-        const taggedUser = message.mentions.users.first();
-        if (!message.mentions.users.size) {
+        const taggedUser = message.mentions.users.first(); // get the first mentioned user
+        if (!message.mentions.users.size) { // if no one is mentioned
             return message.reply('you need to tag a user in order to kick them!');
         }
-        else {message.channel.send(`Kicked ${taggedUser} (demo)`);}
+        else {message.channel.send(`Kicked ${taggedUser} (demo)`);} // kick the mentioned user
+    }
+    else if (command === 'avatar') { // "avatar" command
+        if (!message.mentions.users.size) { // if no one is mentioned
+            // display the user's avatar (png format or gif format if animated)
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+        }
+        const avatarList = message.mentions.users.map(user => { // get mentioned users
+            // get avatars of the mentioned user
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+        });
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
     }
 
     ////
