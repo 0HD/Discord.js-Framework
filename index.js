c333ec2837a7584c45271eaf2c217c2c7e2542b5
0HD <64617192+0HD@users.noreply.github.com>
2020-11-16 16:12:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -44,7 +44,8 @@ client.on('message', message => {
     if (!client.commands.has(command)) return;
     try {
         client.commands.get(command).execute(message, args);
-    } catch (error) {
+    }
+    catch (error) {
         console.error(error);
         message.reply('there was an error trying to execute that command!');
     }
