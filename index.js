63f8cb9deecb4917b3c15f06c279177697d94a3e
0HD <64617192+0HD@users.noreply.github.com>
2020-11-16 14:46:45 +0300

    Fixed bulk-delete limits

--- index.js
+++ index.js
@@ -90,17 +90,18 @@ client.on('message', message => {
         // gets a number from the first argument
         // adds 1 to also delete the user's message with the command
         const amount = parseInt(args[0]) + 1;
-        if (isNaN(amount)) { // check if a real number was provided
+        if (isNaN(amount)) { // stop if the first argument is not a number
             return message.reply('that doesn\'t seem to be a valid number.');
-        }
-        else if (amount > 1 || amount < 12) { // check if the number provided is supported
-            message.channel.bulkDelete(amount, true).catch(err => { // bulk-delete / catch any errors
-                console.error(err);
-                message.channel.send('There was an error trying to prune messages in this channel!');
-            });
-        }
-        else { // if the number isn't supported
-            return message.reply('you need to input a number between 1 and 10.');
+
+        // eslint-disable-next-line brace-style
+        } else if (amount <= 1 || amount > 100) { // stop if number is unsupported
+            return message.reply('you need to input a number between 1 and 99.');
+
+        // eslint-disable-next-line brace-style
+        } else {message.channel.bulkDelete(amount, true).catch(err => { // delete the messages
+            console.error(err); // catch any errors in console
+            message.channel.send('there was an error trying to prune messages in this channel!');
+        });
         }
     }
     ////
